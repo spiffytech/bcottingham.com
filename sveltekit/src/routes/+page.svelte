4e3cf7ac2730f7476cbc82f3c6cc5f7d6e40ed6f
spiffytech <git@spiffy.tech>
2023-11-16 10:37:55 -0500

    Initialized a basic Sveltekit application. Included some fonts and played around with styling the header.

---
+++ sveltekit/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import '../../node_modules/bootstrap-icons/font/bootstrap-icons.min.css';
+</script>
+
+<div class="mx-4 my-2">
+	<div class="flex items-baseline gap-x-8">
+		<h1 class="font-rh text-3xl">Brian Cottingham</h1>
+		<span class="font-src-sans">
+			<a href="mailto:brian@bcottingham.com" class="whitespace-nowrap">
+				<i class="bi-envelope-at"></i>
+				<span class="underline print:no-underline">brian@bcottingham.com</span>
+			</a>
+			|
+			<a href="tel:+1-919-259-8970" class="whitespace-nowrap">
+				<i class="bi-telephone"></i>
+				<span class="underline print:no-underline">919-259-8970</span></a
+			>
+			|
+			<a href="https://github.com/spiffytech" class="whitespace-nowrap">
+				<i class="bi-braces"></i>
+				<span class="underline print:no-underline">github.com/spiffytech<span></span></span></a
+			>
+			|
+			<a href="https://spiffy.tech" class="whitespace-nowrap">
+				<i class="bi-pen"></i>
+				<span
+					class="underline
+			print:no-underline">spiffy.tech</span
+				></a
+			>
+		</span>
+	</div>
+</div>
